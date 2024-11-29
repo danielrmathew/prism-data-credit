13ae736c745bfa5c7704814e93c1decb1afb1f9a
Daniel Mathew <drmathew@ucsd.edu>
2024-11-29 09:58:57 +0000

    added docstrings

--- src/build/predict_llm.py
+++ src/build/predict_llm.py
@@ -1,21 +1,35 @@
 
-
 def predict_bert(pipe, X):
+    """
+    Makes predictions using a finetuned DistilBert model
+
+    Args:
+        pipe (transformers.TextClassificationPipeline): DistilBert model pipeline for inference
+        X (list): List of memos
+
+    Returns:
+        list: Category predictions
+    """
 
     preds = pipe(X)
     preds = [dct['label'] for dct in preds]
-
-    # make_confusion_matrix(y_train, train_preds, model_type, train=True)
-    # make_confusion_matrix(y_test, test_preds, model_type, train=False)
-
+    
     return preds
 
 def predict_fasttext(model, X):
+    """
+    Makes predictions using a finetuned DistilBert model
+
+    Args:
+        pipe (fasttext): fasttext model object
+        X (list): List of memos
+
+    Returns:
+        list: Category predictions
+    """
+    
     preds = []
     for i in range(len(X)):
         preds.append(model.predict(X[i])[0][0])
 
     return preds
-
-    # train_acc = (np.array(train_preds) == np.array(y_train)).mean()
-    # test_acc = (np.array(test_preds) == np.array(y_test)).mean()
